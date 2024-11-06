8d228d189d10caf0cf140856c7d00f7c6d080180
gabo <gabriel.gaboardi13@gmail.com>
2024-11-06 01:39:51 -0300

    added basic packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    icon({
+      include: {
+        "fa6-solid": ["*"], // (Default) Loads entire Material Design Icon set
+      },
+    }),
+  ],
+});
